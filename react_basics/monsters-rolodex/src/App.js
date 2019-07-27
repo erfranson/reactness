1f220ec81c9edc53f93839b6eae05bf93181ebfc
Erfranson <Ethanfranson2@gmail.com>
2019-07-27 13:11:09 -0600

    importing react

---
+++ react_basics/monsters-rolodex/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      monsters: [
+      
+      ]
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(user => this.setState({ monsters: user}));
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+      </div>
+    );
+  }
+}
+export default App;
