232f1c59b5aa2b1086dce8cd9a8be120c788bdd2
Ethan Franson <ethan.franson@suse.com>
2019-08-06 15:29:21 -0600

    Updates

---
+++ precache-manifest.fce2950fad1f2174d565406e36403ebc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbafadee26815633d57689f7856ec7e",
+    "url": "/reactness/react_basics/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c54adde3cd7ddcf44a95",
+    "url": "/reactness/react_basics/monsters-rolodex/static/css/main.fb7cca4b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/reactness/react_basics/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c54adde3cd7ddcf44a95",
+    "url": "/reactness/react_basics/monsters-rolodex/static/js/main.fa68744c.chunk.js"
+  },
+  {
+    "revision": "a7c3a53ef087c6bd3289",
+    "url": "/reactness/react_basics/monsters-rolodex/static/js/runtime~main.359f4376.js"
+  }
+]);
